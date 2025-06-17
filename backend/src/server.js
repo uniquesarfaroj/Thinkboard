41cc171d94bb37b2c1afa6a036816849bc5970fc
Mohammad Sarfaroj <163388114+uniquesarfaroj@users.noreply.github.com>
2025-06-18 00:18:17 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ import notesRoutes  from './routes/notesRoutes.js';
 import { connectDB } from "./config/db.js";
 
 import dotenv from "dotenv";
+import path from "path"
+
 
 import ratelimiter from "./middleware/rateLimiter.js";
 
@@ -14,18 +16,20 @@ const app = express();
 
 const PORT = process.env.PORT  || 5001;
 
+const __dirname = path.resolve()
+
 dotenv.config();
 
 //middleware
-
+if(process.env.NODE_ENV !== "production"){
 app.use(
   cors({
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
-);
-app.options("*", cors());
+);}
+
 
 
 app.use(express.json())
@@ -33,6 +37,13 @@ app.use(express.json())
 app.use(ratelimiter)
 
 app.use("/api/notes",notesRoutes )
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+if(process.env.NODE_ENV === "production"){
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})}
 
 // enable pre-flight request for this route
 
